59d737e13d8ca535b92a424537fdf21700c2e52c
Lily Cant <Lily_betts@hotmail.com>
2021-10-10 20:30:54 +1000

    Added styling for 404 page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
 const Error = () => {
     return ( 
         <>
+        <section className="error" style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", height: "100vh"}}>
             <h1>Error 404: Page Not Found</h1>
+            <Link to="/" style={{ backgroundColor: "#00b392", padding: 10, color: "white", marginTop: 20}}>Home</Link>
+        </section>
         </>
      )
 }
